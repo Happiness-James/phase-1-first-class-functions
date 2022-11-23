1c5a62466405ed93527b5296a5c438af4ae22d6c
Happiness-James <gesarehjames@gmail.com>
2022-11-23 15:54:29 +0000

    commit tree a18e15cf5efd6e166df65873a02bf03464f16045

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ function returnsANamedFunction(){
 return (function named() { return "This is a named function" });
 }
 function returnsAnAnonymousFunction(){
-    return (function () { return "This is an anonymous function" }); 
+    // return (function () { return "This is an anonymous function" }); ?
+    const funcname = fancyName();
+    return funcname;
 }
+// const returnFirstTwoDrivers = function(drivers){return drivers[0,1]}
 
-
+function fancyName() {
+    return "";
+}
